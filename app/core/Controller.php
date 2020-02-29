5800d34bcf6e1ba10dc4c002218dfb3c092a90b4
Cheak <saumonminiball@gmail.com>
2020-02-29 21:36:22 +0200

    Deleted useless code.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,21 +3,12 @@
 
 class Controller
 {
-    private $model;
-
-    public function __construct()
-    {
-        $this->model = new BasicModel();
-    }
 
     public function init()
     {
 
         $data['header'] = Router::render('layouts/header',
-            [
-                'styles' => ['bootstrap.min', 'main'],
-                'nameUser' => $this->model->nameUser()
-            ]);
+            ['styles' => ['bootstrap.min', 'main'],]);
 
         $data['footer'] = Router::render('layouts/footer',
             ['scripts' => ['main']]);
